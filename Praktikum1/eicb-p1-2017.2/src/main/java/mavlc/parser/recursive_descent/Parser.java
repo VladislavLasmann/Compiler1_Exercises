488ce3f0bac695f0b7ead1e8200a1111a6b7671d
Robin Ferrari <seed@seedOS>
2017-11-16 18:04:22 +0100

    see before

--- Praktikum1/eicb-p1-2017.2/src/main/java/mavlc/parser/recursive_descent/Parser.java
+++ Praktikum1/eicb-p1-2017.2/src/main/java/mavlc/parser/recursive_descent/Parser.java
@@ -355,23 +355,31 @@ public final class Parser {
 		 */
 		LeftHandIdentifier leftHandIdentifier = new LeftHandIdentifier(line, column, name);
 
-		if( currentToken.type == AT ){
-			acceptIt();																						// '@'
-			String elementID = accept(ID);																	// ID
-			leftHandIdentifier = new RecordLHSIdentifier(line, column, name, elementID);					// assignment to an record-element
+        switch(currentToken.type) {
+            case AT:
+                acceptIt();																						// '@'
+                String elementID = accept(ID);																	// ID
+                leftHandIdentifier = new RecordLHSIdentifier(line, column, name, elementID);					// assignment to an record-element
+                break;
 
-		} else if( currentToken.type == LBRACKET){
-			acceptIt();																						// '['
-			Expression firstIndex = parseExpr();															// expr
-			leftHandIdentifier = new VectorLHSIdentifier(line, column, name, firstIndex);					// one expression in brackets means vector
-			accept(RBRACKET);																				// ']'
+            case LBRACKET:
+                acceptIt();																						// '['
+                Expression firstIndex = parseExpr();															// expr
+                leftHandIdentifier = new VectorLHSIdentifier(line, column, name, firstIndex);					// one expression in brackets means vector
+                accept(RBRACKET);																				// ']'
 
-			if(currentToken.type == LBRACKET){
-				acceptIt();
-				Expression secondIndex = parseExpr();
-				leftHandIdentifier = new MatrixLHSIdentifier(line, column, name, firstIndex, secondIndex);	// 2x expression in brackets means matrix
-				accept(RBRACKET);
-			}
+                if(currentToken.type == LBRACKET){
+                    acceptIt();
+                    Expression secondIndex = parseExpr();
+                    leftHandIdentifier = new MatrixLHSIdentifier(line, column, name, firstIndex, secondIndex);	// 2x expression in brackets means matrix
+                    accept(RBRACKET);
+                }
+
+            case ASSIGN:
+                break;
+
+            default: 
+                throw new SyntaxError(currentToken, AT, LBRACKET, ASSIGN);
 		}
 
 		accept(ASSIGN);																						// '='
